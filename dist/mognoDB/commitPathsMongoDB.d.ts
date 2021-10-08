f7584a5008ed01cb57ac3c999ed6a5d7c6582f22
Robert Klinger <robert@k1inger.de>
2021-10-08 17:00:39 +0200

    3.6.0

--- dist/mognoDB/commitPathsMongoDB.d.ts
+++ dist/mognoDB/commitPathsMongoDB.d.ts
@@ -1,7 +1,7 @@
 import { MongoDBConfig } from "./mongoDBConfig";
-import { DB } from "bugfinder-framework/dist/00-shared/db/DB";
+import { DB, WriteMode } from "bugfinder-framework/dist/00-shared/db/DB";
 import { CommitPath } from "bugfinder-localityrecorder-commitpath";
-import { LocalityMap, DatasetAP, DatasetAFE } from "bugfinder-framework";
+import { DatasetAFE, DatasetAP, LocalityMap } from "bugfinder-framework";
 import { Logger } from "ts-logger";
 export declare class CommitPathsMongoDB<Annotation, Quantification> implements DB<CommitPath, Annotation, Quantification> {
     private logger;
@@ -15,8 +15,9 @@ export declare class CommitPathsMongoDB<Annotation, Quantification> implements D
      * Writes localities to DB at location (collection/table/file/...) toID. With mode = "a" data will be appended.
      * @param localities
      * @param toID
+     * @param mode
      */
-    writeLocalities(localities: CommitPath[], toID: string, mode?: string): Promise<void>;
+    writeLocalities(localities: CommitPath[], toID: string, mode?: WriteMode): Promise<void>;
     readAnnotations(fromID: string, skip?: number, n?: number): Promise<LocalityMap<CommitPath, Annotation>>;
     /**
      * Writes annotation to DB at location (collection/table/file/...) toID. With mode = "a" data will be appended.
@@ -24,31 +25,34 @@ export declare class CommitPathsMongoDB<Annotation, Quantification> implements D
      * @param toID
      * @param mode
      */
-    writeAnnotations(annotations: LocalityMap<CommitPath, Annotation>, toID: string, mode?: string): Promise<void>;
+    writeAnnotations(annotations: LocalityMap<CommitPath, Annotation>, toID: string, mode?: WriteMode): Promise<void>;
     readQuantifications(fromID: string, skip?: number, n?: number): Promise<LocalityMap<CommitPath, Quantification>>;
     /**
      * Writes quantifications to DB at location (collection/table/file/...) toID. With mode = "a" data will be appended.
      * @param quantifications
      * @param toID
+     * @param mode
      */
-    writeQuantifications(quantifications: LocalityMap<CommitPath, Quantification>, toID: string, mode?: string): Promise<void>;
+    writeQuantifications(quantifications: LocalityMap<CommitPath, Quantification>, toID: string, mode?: WriteMode): Promise<void>;
     readDatasetAP(fromID: string): Promise<DatasetAP>;
     /**
      * Writes DatasetAP to DB at location (collection/table/file/...) toID. With mode = "a" data will be appended.
      * @param toID
      * @param dataset
+     * @param mode
      */
-    writeDatasetAP(toID: string, dataset: DatasetAP, mode?: string): Promise<void>;
+    writeDatasetAP(toID: string, dataset: DatasetAP, mode?: WriteMode): Promise<void>;
     readDatasetAFE(fromID: string): Promise<DatasetAFE>;
     /**
      * Writes DatasetAFE to DB at location (collection/table/file/...) toID. With mode = "a" data will be appended.
      * @param toID
      * @param dataset
+     * @param mode
      */
-    writeDatasetAFE(toID: string, dataset: DatasetAFE, mode?: string): Promise<void>;
+    writeDatasetAFE(toID: string, dataset: DatasetAFE, mode?: WriteMode): Promise<void>;
     private read;
-    write(obj: any, toID: string, mode?: string): Promise<void>;
-    writeMany(objs: any[], toID: string, mode?: string): Promise<void>;
+    write(obj: any, toID: string, mode?: WriteMode): Promise<void>;
+    writeMany(objs: any[], toID: string, mode?: WriteMode): Promise<void>;
     /**
      * Returns true if collection is empty. Logs error if error is set to true
      * @param toID
