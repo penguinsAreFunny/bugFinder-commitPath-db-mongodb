3cd442e354b3ddd011598035d5eb8c33a25e1346
Robert Klinger <robert@k1inger.de>
2021-10-12 19:15:02 +0200

    3.6.1

--- dist/TYPES.d.ts
+++ dist/TYPES.d.ts
@@ -1,4 +1,5 @@
 export declare const BUGFINDER_DB_COMMITPATH_MONGODB_TYPES: {
     mongoDBConfig: symbol;
     logger: symbol;
+    logConfig: symbol;
 };
--- dist/TYPES.js
+++ dist/TYPES.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.BUGFINDER_DB_COMMITPATH_MONGODB_TYPES = void 0;
 exports.BUGFINDER_DB_COMMITPATH_MONGODB_TYPES = {
     mongoDBConfig: Symbol("MongoDBConfig"),
-    logger: Symbol("Logger")
+    logger: Symbol("Logger"),
+    logConfig: Symbol("LogConfig")
 };
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiVFlQRVMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvVFlQRVMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQWEsUUFBQSxxQ0FBcUMsR0FBRztJQUNqRCxhQUFhLEVBQUUsTUFBTSxDQUFDLGVBQWUsQ0FBQztJQUN0QyxNQUFNLEVBQVMsTUFBTSxDQUFDLFFBQVEsQ0FBQztDQUNsQyxDQUFBIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiVFlQRVMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvVFlQRVMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQWEsUUFBQSxxQ0FBcUMsR0FBRztJQUNqRCxhQUFhLEVBQUUsTUFBTSxDQUFDLGVBQWUsQ0FBQztJQUN0QyxNQUFNLEVBQVMsTUFBTSxDQUFDLFFBQVEsQ0FBQztJQUMvQixTQUFTLEVBQU8sTUFBTSxDQUFDLFdBQVcsQ0FBQztDQUN0QyxDQUFBIn0=
