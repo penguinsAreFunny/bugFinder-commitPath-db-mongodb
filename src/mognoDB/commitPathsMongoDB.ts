b974f4282bce8fe9e50a4ea222eac918b3d8eb6b
Robert Klinger <robert@k1inger.de>
2021-10-24 21:46:29 +0200

    feat: writing large Dataset as streams as MongoDB does not support storing files with more than 16MB with default method.

--- src/mognoDB/commitPathsMongoDB.ts
+++ src/mognoDB/commitPathsMongoDB.ts
@@ -1,13 +1,14 @@
 import {inject, injectable} from "inversify";
-import {MongoClient} from "mongodb";
+import {MongoClient, GridFSBucket} from "mongodb";
 import {MongoDBConfig} from "./mongoDBConfig";
-import {DB, WriteMode} from "bugfinder-framework/dist/00-shared/db/DB";
 import {CommitPath} from "bugfinder-localityrecorder-commitpath";
 import {BUGFINDER_DB_COMMITPATH_MONGODB_TYPES} from "../TYPES";
 import {Commit} from "bugfinder-localityrecorder-commit";
-import {DatasetAFE, DatasetAP, LocalityMap, SHARED_TYPES} from "bugfinder-framework";
-import {Logger} from "ts-logger"
+import {DatasetAFE, DatasetAP, LocalityMap, DB, WriteMode, SHARED_TYPES} from "bugfinder-framework";
+import {Logger} from "ts-log"
+import {Readable} from "stream";
 
+const COLLECTION_FILES_APPENDIX = ".files"
 const COMMIT_LOCATION_PREFIX = "__COMMITS__";
 
 @injectable()
@@ -150,33 +151,40 @@ export class CommitPathsMongoDB<Annotation, Quantification> implements DB<Commit
     }
 
     async readDatasetAP(fromID: string): Promise<DatasetAP> {
-        const dataset = (await this.read(fromID))[0]
-        return dataset[0]
+        this.logger?.info(`Reading datasetAP from collection ${fromID} using database ${this.dbConfig.dbName} ` +
+            `from ${this.dbConfig.url}...`)
+        return await this.readLarge(fromID)
     }
 
     /**
-     * Writes DatasetAP to DB at location (collection/table/file/...) toID. With mode = "a" data will be appended.
+     * Writes DatasetAP to DB at location (collection/table/file/...) toID.
      * @param toID
      * @param dataset
      * @param mode
      */
-    async writeDatasetAP(toID: string, dataset: DatasetAP, mode?: WriteMode): Promise<void> {
-        await this.write(dataset, toID, mode)
+    async writeDatasetAP(toID: string, dataset: DatasetAP): Promise<void> {
+        this.logger?.info(`Writing datasetAP to collection ${toID} using database ` +
+            `${this.dbConfig.dbName} from ${this.dbConfig.url}...`)
+        if (!await this.empty(toID + COLLECTION_FILES_APPENDIX, true))
+            return
+        await this.writeLarge(dataset, toID)
     }
 
     async readDatasetAFE(fromID: string): Promise<DatasetAFE> {
-        const dataset = (await this.read(fromID))[0]
-        return dataset[0]
+        return await this.readLarge(fromID)
     }
 
     /**
-     * Writes DatasetAFE to DB at location (collection/table/file/...) toID. With mode = "a" data will be appended.
+     * Writes DatasetAFE to DB at location (collection/table/file/...) toID.
      * @param toID
      * @param dataset
-     * @param mode
      */
-    async writeDatasetAFE(toID: string, dataset: DatasetAFE, mode?: WriteMode): Promise<void> {
-        await this.write(dataset, toID, mode)
+    async writeDatasetAFE(toID: string, dataset: DatasetAFE): Promise<void> {
+        this.logger?.info(`Writing datasetAFE to collection ${toID} using database ` +
+            `${this.dbConfig.dbName} from ${this.dbConfig.url}...`)
+        if (!await this.empty(toID + COLLECTION_FILES_APPENDIX, true))
+            return
+        await this.writeLarge(dataset, toID)
     }
 
     private async read(fromID: string, skip?: number, n?: number): Promise<any[]> {
@@ -194,6 +202,65 @@ export class CommitPathsMongoDB<Annotation, Quantification> implements DB<Commit
 
         await client.close();
         return dbContent;
+    }
+
+    /**
+     * Writes a large (> 16 MB) obj to Database.
+     * @see http://mongodb.github.io/node-mongodb-native/3.0/tutorials/gridfs/
+     * @param obj
+     * @param toID
+     */
+    async writeLarge(obj: any, toID: string) {
+        // examples: https://www.tabnine.com/code/javascript/classes/mongodb/GridFSBucket
+        // @formatter:off
+        const client: MongoClient   = await MongoClient.connect(this.dbConfig.url);
+        const db                    = client.db(this.dbConfig.dbName);
+        //const collectionName        = toID;
+        //const collection            = db.collection(collectionName);
+        // @formatter:on
+
+
+        const readable = Readable.from(JSON.stringify(obj))
+        const bucket = new GridFSBucket(db, {bucketName: toID});
+        console.log("Writing to db")
+        return new Promise((resolve, reject) => {
+            readable.pipe(bucket.openUploadStream(toID))
+                .on('finish', () => {
+                    console.log('done!');
+                    client.close()
+                    resolve(true)
+                })
+                .on("error", (error) => {
+                    reject(error)
+                })
+        })
+    }
+
+    async readLarge(fromID: string): Promise<any> {
+        // examples: https://www.tabnine.com/code/javascript/classes/mongodb/GridFSBucket
+        // @formatter:off
+        const client: MongoClient   = await MongoClient.connect(this.dbConfig.url);
+        const db                    = client.db(this.dbConfig.dbName);
+        // @formatter:on
+
+        const bucket = new GridFSBucket(db, {bucketName: fromID});
+        const stream = bucket.openDownloadStreamByName(fromID)
+        return new Promise((resolve, reject) => {
+            const chunks = []
+            stream.on("data", data => {
+                chunks.push(data)
+            })
+            stream.on("end", () => {
+                const data = Buffer.concat(chunks)
+                const dataset = JSON.parse(data.toString("utf-8"))
+                client.close()
+                resolve(dataset)
+            })
+            stream.on("error", err => {
+                reject(err)
+            })
+            stream.read()
+        })
     }
 
     async write(obj: any, toID: string, mode?: WriteMode) {
@@ -253,6 +320,7 @@ export class CommitPathsMongoDB<Annotation, Quantification> implements DB<Commit
     private async empty(toID: string, error: boolean): Promise<boolean> {
         const elementsInCollection = await this.read(toID);
         const numberElInCol = elementsInCollection.length
+        console.log("JUP", numberElInCol, " ", toID)
         if (numberElInCol > 0) {
             if (error) {
                 this.logger?.error(`Found ${numberElInCol} elements in database collection ${toID}.
@@ -276,3 +344,4 @@ export class CommitPathsMongoDB<Annotation, Quantification> implements DB<Commit
     }
 
 }
+
